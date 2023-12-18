e3f34b1232f40935af2a334e0fc9ababea5c6738
vmp78 <manhphuocvu03@gmail.com>
2023-12-18 20:43:40 +0700

    vdweqgy

--- src/routes/index.js
+++ src/routes/index.js
@@ -9,6 +9,7 @@ function route(app) {
     app.use('/pop', PopRouter);
     app.use('/accom', AccomRouter);
     app.use('/payment', PayRouter);
+    app.use('/payment', PayRouter);
     app.use('/', siteRouter);
 }
 
