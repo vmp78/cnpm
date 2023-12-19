f79d9d85f83d7c7bb214a3b9862934bc86b7138f
TheCoTam <hoangnguyenanhtuyen@gmail.com>
2023-12-19 17:11:40 +0700

    Su manh len nao

--- src/app/controllers/SiteController.js
+++ src/app/controllers/SiteController.js
@@ -3,8 +3,6 @@ const session = require('express-session');
 class SiteController {
     // [GET] /
     home(req, res, next) {
-        console.log(req.session.info)
-        console.log(res.locals.headerData)
         var info = req.session.info || 'none'
         res.render('home', { info: info })
     }
--- src/routes/me.js
+++ src/routes/me.js
@@ -5,6 +5,6 @@ const meController = require('../app/controllers/MeController');
 
 router.get('/login', meController.login);
 router.post('/login', meController.identyfy);
-router.post('/logout', meController.logout);
+router.get('/logout', meController.logout);
 
 module.exports = router;
