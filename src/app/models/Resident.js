80916a25f03b2348b164f1dac3b7d95244c5d851
TheCoTam <hoangnguyenanhtuyen@gmail.com>
2023-12-27 23:57:49 +0700

    HTA

--- src/app/models/Resident.js
+++ src/app/models/Resident.js
@@ -1,11 +1,12 @@
 const mongoose = require('mongoose');
+const unidecode = require('unidecode');
 //const slug = require('mongoose-slug-generator');
 // const slug = require('mongoose-slug-plugin');
 
 const Schema = mongoose.Schema;
 
 const Resident = new Schema({
-    Id: { type: String, required: true },
+    Id: { type: String },
     name: { type: String, required: true },
     age: { type: String, required: true },
     gender: { type: String, required: true },
@@ -13,6 +14,12 @@ const Resident = new Schema({
     email: { type: String },
     houseId: { type: String, required: true },
     isHouseholder: { type: Boolean, required: true },
+});
+
+Resident.pre('save', function(next) {
+    this.Id = unidecode(this.name).toLowerCase().replace(/\s+/g, '').toLowerCase().replace(/\s+/g, '');
+
+    next();
 });
 
 module.exports = mongoose.model('Resident', Resident);
