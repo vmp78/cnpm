37b39ef7490fd8125d57bb5fc6ef305f01e5f60a
TTP03 <130140359+TTP03@users.noreply.github.com>
2024-01-12 15:42:13 +0700

    connection

--- src/config/db/index.js
+++ src/config/db/index.js
@@ -1,12 +1,14 @@
 const mongoose = require('mongoose');
 
+const uri = 'mongodb+srv://ttphong148:X2vSu7PnxHmruLfj@phong.gsxpxuh.mongodb.net/?retryWrites=true&w=majority';
+
 async function connect() {
     try {
-        await mongoose.connect('mongodb://127.0.0.1:27017/the_co_tam');
-        console.log('Connect successfully!!!');
+        await mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+        console.log('Kết nối thành công!!!');
     } catch (error) {
-        console.log('Connect failue!!!');
-        console.log(error.stack);
+        console.log('Kết nối thất bại!!!');
+        console.error(error);
     }
 }
 
